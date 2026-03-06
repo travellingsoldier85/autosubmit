d471b62e4204aa479fb31f9abb6815713ca48a1f
dbeltrankyl <daniel.beltran@bsc.es>
2026-03-06 13:26:51 +0100

    fixes #2101

--- autosubmit/job/job.py
+++ autosubmit/job/job.py
@@ -248,7 +248,7 @@ class Job(object):
         self.check = 'true'
         self.check_warnings = False
         self.packed = False
-        self.hold = False # type: bool
+        self.hold = False  # type: bool
         self.distance_weight = 0
         self.level = 0
         self._export = "none"
@@ -2160,6 +2160,30 @@ class Job(object):
         parameters['DELETE_WHEN_EDGELESS'] = self.delete_when_edgeless
         parameters = self.calendar_chunk(parameters)
         parameters = self.calendar_split(as_conf, parameters, set_attributes)
+        # https://github.com/BSC-ES/autosubmit/issues/2101#issuecomment-4011405437
+        if parameters.get('CHUNK_START_DATE', None) and parameters.get('SPLIT_START_DATE', None):
+            parameters['JOB_START_DATE'] = parameters.get('SPLIT_START_DATE', parameters['CHUNK_START_DATE'])
+            parameters['JOB_END_DATE'] = parameters.get('SPLIT_END_DATE', parameters['CHUNK_START_DATE'])
+            parameters['JOB_START_YEAR'] = parameters.get('SPLIT_START_YEAR', parameters['CHUNK_START_YEAR'])
+            parameters['JOB_START_MONTH'] = parameters.get('SPLIT_START_MONTH', parameters['CHUNK_START_MONTH'])
+            parameters['JOB_START_DAY'] = parameters.get('SPLIT_START_DAY', parameters['CHUNK_START_DAY'])
+            parameters['JOB_START_HOUR'] = parameters.get('SPLIT_START_HOUR', parameters['CHUNK_START_HOUR'])
+            parameters['JOB_END_YEAR'] = parameters.get('SPLIT_END_YEAR', parameters['CHUNK_END_YEAR'])
+            parameters['JOB_END_MONTH'] = parameters.get('SPLIT_END_MONTH', parameters['CHUNK_END_MONTH'])
+            parameters['JOB_END_DAY'] = parameters.get('SPLIT_END_DAY', parameters['CHUNK_END_DAY'])
+            parameters['JOB_END_HOUR'] = parameters.get('SPLIT_END_HOUR', parameters['CHUNK_END_HOUR'])
+        elif parameters.get('CHUNK_START_DATE', None):
+            parameters['JOB_START_DATE'] = parameters['CHUNK_START_DATE']
+            parameters['JOB_END_DATE'] = parameters['CHUNK_END_DATE']
+            parameters['JOB_START_YEAR'] = parameters['CHUNK_START_YEAR']
+            parameters['JOB_START_MONTH'] = parameters['CHUNK_START_MONTH']
+            parameters['JOB_START_DAY'] = parameters['CHUNK_START_DAY']
+            parameters['JOB_START_HOUR'] = parameters['CHUNK_START_HOUR']
+            parameters['JOB_END_YEAR'] = parameters['CHUNK_END_YEAR']
+            parameters['JOB_END_MONTH'] = parameters['CHUNK_END_MONTH']
+            parameters['JOB_END_DAY'] = parameters['CHUNK_END_DAY']
+            parameters['JOB_END_HOUR'] = parameters['CHUNK_END_HOUR']
+
         parameters['NUMMEMBERS'] = len(as_conf.get_member_list())
 
         parameters['JOB_DEPENDENCIES'] = self.dependencies
